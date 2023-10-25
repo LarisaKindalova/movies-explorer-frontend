4d4e8ac6ae989b5560f4cdaa09363739f0e47dcd
Larisa Kindalova <l.kindalova@yandex.ru>
2023-10-25 19:15:25 +1000

    fix: sameSite

--- src/utils/MainApi.jsx
+++ src/utils/MainApi.jsx
@@ -4,17 +4,100 @@ function checkResponse(res) {
   if (res.ok) {
     return res.json();
   }
-  return Promise.reject(`Ошибка: ${res.status}`);
+  return Promise.reject({
+    status: res.status,
+    message:`Ошибка: ${res.status}`});
 }
 
-export function register (name, email, password) {
+export function register (data) {
+  console.log(data);
   return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
       "Accept": "application/json",
-      "Context-type": "application/json",
+      "Content-type": "application/json",
     },
     credentials: "include",
-    body: JSON.stringify({name, email, password}),
+    body: JSON.stringify(data),
   }).then(res => checkResponse(res));
 }
+
+export function login (data) {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  }).then(res => checkResponse(res));
+}
+
+export function checkToken () {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+  }).then((res) => checkResponse(res));
+};
+  
+export function updateUserInfo(user) {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "PATCH",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+    body: JSON.stringify(user),
+  }).then((res) => checkResponse(res));
+};
+
+export function getMovies() {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+  }).then((res) => checkResponse(res));
+};
+
+export function saveMovies(movie) {
+  return fetch(`${BASE_URL}/movies`, {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+    body: JSON.stringify(movie)
+  }).then((res) => checkResponse(res));
+};
+
+export function deleteMovies(movieId) {
+  return fetch(`${BASE_URL}/movies/${movieId}`, {
+    method: "DELETE",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+  }).then((res) => checkResponse(res));
+};
+
+export function logOut () {
+  return fetch(`${BASE_URL}/signout`, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: 'include',
+  }).then(res => checkResponse(res));
+};
